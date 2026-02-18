202384a82a52a32da327a7a861a841a87fa0749a
DoYoung Kang <dooly386@gmail.com>
2026-02-18 17:06:35 +0900

    My commit message

--- src/components/DocBanner.js
+++ src/components/DocBanner.js
@@ -1,15 +1,15 @@
 import React from 'react';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function DocBanner() {
+  const bannerUrl = useBaseUrl('/img/ai-banner.png');
+
   return (
     <div style={{marginBottom: '2rem'}}>
       <img
-        src="/img/ai-banner.png"
+        src={bannerUrl}
         alt="imEDA Banner"
-        style={{
-          width: '100%',
-          borderRadius: '12px',
-        }}
+        style={{width: '100%', borderRadius: '12px'}}
       />
     </div>
   );
