bcb74a324499b36a2a6f599f65f34d0c38df3f83
DoYoung Kang <dooly386@gmail.com>
2026-02-18 13:07:41 +0900

    Fix HomepageFeatures component

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -32,19 +32,17 @@ const FeatureList = [
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({title, description}) {
   return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
+    <div className="col col--4">
+      <div style={{padding: '1rem'}}>
+        <h3>{title}</h3>
         <p>{description}</p>
       </div>
     </div>
   );
 }
+
 
 export default function HomepageFeatures() {
   return (
