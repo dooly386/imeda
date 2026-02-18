039ee38e3219f7bfb73518e027776acf9cecfcd5
DoYoung Kang <dooly386@gmail.com>
2026-02-18 16:51:43 +0900

    My commit message

--- docs/index.md
+++ docs/index.md
@@ -3,8 +3,9 @@ title: imEDA Documentation
 sidebar_position: 1
 ---
 
-<img src="/img/ai-banner.png" alt="imEDA Banner" style="width:100%; border-radius:12px; margin-bottom:2rem;" />
+import Banner from '@site/src/components/DocBanner';
 
+<Banner />
 
 # imEDA : EDA 가 AI를 만나는 순간
 
---
+++ src/components/DocBanner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function DocBanner() {
+  return (
+    <div style={{marginBottom: '2rem'}}>
+      <img
+        src="/img/ai-banner.png"
+        alt="imEDA Banner"
+        style={{
+          width: '100%',
+          borderRadius: '12px',
+        }}
+      />
+    </div>
+  );
+}
